2c2b641577a617754d8bbe994ac3ceaf94c9543c
sbear123 <sbear123@dgsw.hs.kr>
2020-10-10 19:33:08 +0900

    [ADD] CheckBean

---
+++ DidYouComeServer/src/bean/CheckBean.java
@@ -0,0 +1,27 @@
+package bean;
+
+public class CheckBean {
+	private String time;
+	private String check;
+	private String name;
+	
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getCheck() {
+		return check;
+	}
+	public void setCheck(String check) {
+		this.check = check;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
