d0d7a7c925514afff00be0e1b6b9a5850af25467
sbear123 <sbear123@dgsw.hs.kr>
2020-10-10 20:58:49 +0900

    [ADD] 이름변경api완성

---
+++ DidYouComeServer/src/bean/DB/NameDBBean.java
@@ -0,0 +1,36 @@
+package bean.DB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class NameDBBean extends CommonDBBean{
+	//Singleton
+	private static NameDBBean instance = new NameDBBean();
+	private NameDBBean() {}
+	public static NameDBBean getInstance() {
+		return instance;
+	}
+	
+	public int change(String userid, String name) {
+		int result = 0;
+		Connection conn = getConnection();
+		if(conn==null) return 0;
+		System.out.println("conn");
+		
+		String sql = "UPDATE user SET name =? WHERE userid = ?;";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, name);
+			pstmt.setString(2, userid);
+			
+			result = pstmt.executeUpdate();
+			if(pstmt!=null) pstmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		closeConnection(conn);
+		return result;
+	}
+}
--- DidYouComeServer/src/dyc/action/api/NameJsonAction.java
+++ DidYouComeServer/src/dyc/action/api/NameJsonAction.java
@@ -1,5 +1,33 @@
 package dyc.action.api;
 
-public class NameJsonAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.IOUtils;
+
+import com.google.gson.Gson;
+
+import bean.ResultBean;
+import bean.UserBean;
+import bean.DB.NameDBBean;
+import dyc.action.Action;
+
+public class NameJsonAction implements Action {
+
+	@Override
+	public String requestProcess(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		request.setCharacterEncoding("UTF-8");
+		Gson gson = new Gson();
+		ResultBean result = new ResultBean();
+		
+		// input
+		String str = IOUtils.toString(request.getReader());
+		UserBean requestUser = gson.fromJson(str, UserBean.class); 
+		
+		int count = NameDBBean.getInstance().change(requestUser.getUserid(), requestUser.getName());
+		if(count==1)
+			result.result="ok";
+		
+		return gson.toJson(result, ResultBean.class);
+	}
 }
